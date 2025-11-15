9937ce18a3b65ac77f5f9364f34b9d60483cce5c
artyomdev250 <artyom.dev250@gmail.com>
2025-11-15 21:53:35 +0500

    refresh token setup

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,9 +1,12 @@
 const express = require('express');
 const router = express.Router();
+
 const { createUser } = require('../controllers/signUpController');
 const { loginUser } = require('../controllers/signInController');
+const { refreshToken } = require('../controllers/refreshTokenController');
 
 router.post('/signup', createUser);
 router.post('/signin', loginUser);
+router.post('/refresh', refreshToken);
 
 module.exports = router;
