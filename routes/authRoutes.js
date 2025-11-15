ce61af66e743e02be30da88d914b52f31065fd78
artyomdev250 <artyom.dev250@gmail.com>
2025-11-16 01:23:57 +0500

    logout setup

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,12 +1,13 @@
 const express = require('express');
 const router = express.Router();
-
 const { createUser } = require('../controllers/signUpController');
 const { loginUser } = require('../controllers/signInController');
 const { refreshToken } = require('../controllers/refreshTokenController');
+const { logoutUser } = require('../controllers/logoutController');
 
 router.post('/signup', createUser);
 router.post('/signin', loginUser);
 router.post('/refresh', refreshToken);
+router.post('/logout', logoutUser);
 
 module.exports = router;
