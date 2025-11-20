978510697a445e7bdf72bca8256b6e128314dd20
artyomdev250 <artyom.dev250@gmail.com>
2025-11-21 01:10:28 +0500

    enable CORS

--- server.js
+++ server.js
@@ -1,10 +1,18 @@
 const express = require('express');
+const cors = require('cors');
 const dotenv = require('dotenv');
 const connectDB = require('./mongodb/connect');
 
 dotenv.config();
 
 const app = express();
+
+// CORS CONFIG
+app.use(cors({
+    origin: process.env.FRONTEND_URL || "http://localhost:5173/",
+    credentials: true,
+}));
+
 app.use(express.json());
 
 // Connect database
