ba82eb728610f29ca51cc560ef0f7cd33ef3bf31
artyomdev250 <artyom.dev250@gmail.com>
2025-11-21 01:15:59 +0500

    allowedHeaders commit

--- server.js
+++ server.js
@@ -11,6 +11,7 @@ const app = express();
 app.use(cors({
     origin: process.env.FRONTEND_URL || "http://localhost:5173/",
     credentials: true,
+    allowedHeaders: ["Content-Type", "Authorization"]
 }));
 
 app.use(express.json());
